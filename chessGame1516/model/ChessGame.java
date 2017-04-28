4abeb258009d1c82db1ad74ae13ada783d21b799
RudyDeal <rudy.deal.rd@gmail.com>
2017-04-28 17:28:23 +0200

    debut DP 7

---
+++ chessGame1516/model/ChessGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Rudy_DEAL
+ */
+public class ChessGame {
+    public String toString();
+    public boolean isEnd();
+    public boolean move(int xInit, int yInit, int xFinal, int yFinal);
+    public String getMessage();
+    public Couleur getColorCurrentPlayer();
+    //TODO : 
+    
+    
+}
